fb7b0741f68d95fd58efdc67e78335c4ba365905
2400320229 <2029771797@qq.com>
2024-11-25 19:19:54 +0800

    ad

--- app/src/main/java/com/example/wordapp/UserFragment.kt
+++ app/src/main/java/com/example/wordapp/UserFragment.kt
@@ -132,7 +132,7 @@ class UserFragment : Fragment() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         startActivityForResult(intent, PICK_IMAGE_REQUEST)
 
-
+        
     }
 
     // 处理图片选择结果
